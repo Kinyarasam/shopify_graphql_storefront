7bcb89a4f01c0f6ee15b3103683116bc05b2a94d
kinyarasam <kinyarasam@macs-MacBook-Pro.local>
2023-10-11 17:25:41 +0300

    Configure the storefront API access

--- config/index.js
+++ config/index.js
@@ -4,6 +4,14 @@ import dotenv from 'dotenv'
  * Load the environment variables.
  */
 dotenv.config();
-const PORT = process.env.PORT;
 
-export default PORT;
+const config = {};
+config.PORT = process.env.PORT;
+
+config.STOREFRONT_ACCESS_TOKEN = process.env.STOREFRONTACCESSTOKEN;
+config.SHOP_URL = process.env.SHOPURL;
+config.SHOPIFY_API_KEY = process.env.SHOPIFYAPIKEY;
+config.SHOPIFY_SCOPES = process.env.SHOPIFYSCOPES.split(', ')
+config.SHOPIFY_API_SECRET = process.env.SHOPIFYAPISECRET;
+
+export default config;
--- server.js
+++ server.js
@@ -1,8 +1,56 @@
 import express from 'express';
-import PORT from './config';
+import config from './config';
+import { Shopify, shopifyApi } from '@shopify/shopify-api';
+import '@shopify/shopify-api/adapters/node'
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log('Server running on PORT: ', PORT)
+const shopify = shopifyApi({
+  apiKey: config.SHOPIFY_API_KEY,
+  apiSecretKey: config.SHOPIFY_API_SECRET,
+  scopes: config.SHOPIFY_SCOPES,
+  hostName: `localhost:${config.PORT}`
+});
+
+const storefrontClient = new shopify.clients.Storefront({
+  session: {
+    accessToken: config.STOREFRONT_ACCESS_TOKEN,
+    apiKey: config.SHOPIFY_API_KEY,
+    apiSecretKey: config.SHOPIFY_API_SECRET,
+    shop: config.SHOP_URL,
+    scope: config.SHOPIFY_SCOPES
+  },
 })
+
+const getProducts = async () => {
+  try {
+    const products = await storefrontClient.query({
+      data: `{
+        products (first: 3) {
+          edges {
+            node {
+              id
+              title
+              description
+            }
+          }
+        }
+      }`,
+    });
+  
+    return products
+  } catch (err) {
+    return err.message;
+  }
+};
+
+getProducts()
+  .then((prod) => console.log(prod))
+  .catch((err) => console.log(err))
+
+
+app.listen(config.PORT, () => {
+  console.log('Server running on PORT: ', config.PORT)
+});
+
+export default app;
